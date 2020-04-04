b466293a9b86df0af5aaf34e40ea1b060e506cc0
Viorel Rinja <viorel.rinja@gmail.com>
2020-04-04 13:26:41 +0300

    Add Vegetale multiplication
    Added a value to CELL_TYPE, which will be used as preserve cell, for fallback assignments, for now.

--- Assets/Scripts/CellTypes.cs
+++ Assets/Scripts/CellTypes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public enum CELL_TYPE
 {
+    PRESERVE = -1,
     NULL = 0,
     PREDATOR = 1,
     PREY = 2,
--- Assets/Scripts/GameRules.cs
+++ Assets/Scripts/GameRules.cs
@@ -6,12 +6,37 @@ public class GameRules : MonoBehaviour
 {
     public static float vegetableSelfSpawnChance = .0001f;
 
+    public static CELL_TYPE[,] step(CELL_TYPE[,] grid)
+    {
+        CELL_TYPE[,] gridCopy = new CELL_TYPE[grid.GetLength(0), grid.GetLength(1)];
+
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                // gridCopy[i, j] = spawnPrey(grid, i, j);
+                gridCopy[i, j] = fallback(selfSpawnVegetable(grid, i, j), grid[i, j]);
+                gridCopy[i, j] = fallback(multiplyVegetable(grid, i, j), gridCopy[i, j]);
+            }
+        }
+
+        return gridCopy;
+    }
+
+    private static CELL_TYPE fallback(CELL_TYPE primary, CELL_TYPE fallback) {
+        if (primary != CELL_TYPE.PRESERVE)
+            return primary;
+        else
+            return fallback;
+    }
+
     private static int getLoopIndex(int index, int max)
     {
         index = index % max;
         return (index >= 0) ? index : max + index;
     }
-    private static int countNeighbours(CELL_TYPE[,] grid, int x, int y)
+
+    private static int countNeighbours(CELL_TYPE[,] grid, int x, int y, CELL_TYPE type)
     {
         int horizontalLimit = grid.GetLength(0);
         int verticalLimit = grid.GetLength(1);
@@ -26,31 +51,16 @@ public class GameRules : MonoBehaviour
                 int safeI = getLoopIndex(i, horizontalLimit);
                 int safeJ = getLoopIndex(j, verticalLimit);
 
-                if (grid[safeI, safeJ] != CELL_TYPE.NULL) counter++;
+                if (grid[safeI, safeJ] == type) ++counter;
             }
         }
 
         return counter;
     }
-    public static CELL_TYPE[,] step(CELL_TYPE[,] grid)
+
+    private static CELL_TYPE spawnPrey(CELL_TYPE[,] grid, int i, int j)
     {
-        CELL_TYPE[,] gridCopy = new CELL_TYPE[grid.GetLength(0), grid.GetLength(1)];
-
-        for (int i = 0; i < grid.GetLength(0); i++)
-        {
-            for (int j = 0; j < grid.GetLength(1); j++)
-            {
-                gridCopy[i, j] = spawnHerbivore(grid, i, j);
-                gridCopy[i, j] = selfSpanPlant(grid, i, j);
-            }
-        }
-
-        return gridCopy;
-    }
-
-    private static CELL_TYPE spawnHerbivore(CELL_TYPE[,] grid, int i, int j)
-    {
-        int neighbours = countNeighbours(grid, i, j);
+        int neighbours = countNeighbours(grid, i, j, CELL_TYPE.PREY);
         if (neighbours == 3)
         {
             return CELL_TYPE.PREY;
@@ -63,11 +73,23 @@ public class GameRules : MonoBehaviour
         return CELL_TYPE.NULL;
     }
 
-    private static CELL_TYPE selfSpanPlant(CELL_TYPE[,] grid, int i, int j)
+    private static CELL_TYPE selfSpawnVegetable(CELL_TYPE[,] grid, int i, int j)
     {
         if (grid[i, j] != CELL_TYPE.NULL) 
-            return grid[i, j];
+            return CELL_TYPE.PRESERVE;
         else
             return (Random.Range(0f, 1f) < vegetableSelfSpawnChance)? CELL_TYPE.VEGETABLE : CELL_TYPE.NULL;
+    }
+
+    private static CELL_TYPE multiplyVegetable(CELL_TYPE[,] grid, int i, int j)
+    {
+        if (grid[i, j] == CELL_TYPE.NULL)
+        {
+            float neighbours = countNeighbours(grid, i, j, CELL_TYPE.VEGETABLE);
+            float chance = neighbours / 8;
+            return (Random.Range(0f, 1f) < chance) ? CELL_TYPE.VEGETABLE : CELL_TYPE.PRESERVE;
+        }
+        else
+            return CELL_TYPE.PRESERVE;
     }
 }
